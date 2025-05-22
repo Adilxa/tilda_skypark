ef52b09aa81654606fca000b8d228ca7f367233c
Adilxa <adilhanimerov82@gmail.com>
2025-05-22 11:09:58 +0600

    feat:deploy

---
+++ dist/page69330901.ba58396a.js
@@ -0,0 +1,15 @@
+function t846_init(recId) {
+    t_onFuncLoad('t_card__moveClickOnCard', function() {
+        t_card__moveClickOnCard(recId);
+    });
+    t_onFuncLoad('t_card__addFocusOnTab', function() {
+        t_card__addFocusOnTab(recId);
+    });
+}
+function t678_onSuccess(form) {
+    t_onFuncLoad('t_forms__onSuccess', function() {
+        t_forms__onSuccess(form);
+    });
+}
+
+//# sourceMappingURL=page69330901.ba58396a.js.map

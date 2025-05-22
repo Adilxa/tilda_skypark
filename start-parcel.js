585419d6cff85776d3d45f71888eca8e2fcbba0e
Adilxa <adilhanimerov82@gmail.com>
2025-05-22 11:32:50 +0600

    feat:parcel

--- start-parcel.js
+++ start-parcel.js
@@ -1,9 +1,12 @@
 // start-parcel.js
 const { spawn } = require("child_process");
 
+// !!! Замените '/path/to/your/npm' на фактический путь, который вы получили от `which npm`
+const npmPath = "/usr/bin/npm"; // Пример, измените на ваш фактический путь
+
 console.log("Starting Parcel via npm run start...");
 
-const parcelProcess = spawn("npm", ["run", "start"], {
+const parcelProcess = spawn(npmPath, ["run", "start"], {
   cwd: __dirname, // Убедитесь, что Parcel запускается из корневой директории проекта
   stdio: "inherit", // Перенаправляем вывод Parcel в консоль PM2
 });

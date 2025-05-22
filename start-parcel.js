8ce13c0cbc1db3da8b9add552930fa162cf8dd66
Adilxa <adilhanimerov82@gmail.com>
2025-05-22 11:29:10 +0600

    feat:parcel

---
+++ start-parcel.js
@@ -0,0 +1,21 @@
+// start-parcel.js
+const { spawn } = require("child_process");
+
+console.log("Starting Parcel via npm run start...");
+
+const parcelProcess = spawn("npm", ["run", "start"], {
+  cwd: __dirname, // Убедитесь, что Parcel запускается из корневой директории проекта
+  stdio: "inherit", // Перенаправляем вывод Parcel в консоль PM2
+});
+
+parcelProcess.on("exit", code => {
+  console.log(`Parcel process exited with code ${code}`);
+  // В продакшене, возможно, стоит перезапускать или логировать ошибку
+  // Здесь просто выходим, чтобы PM2 мог управлять перезапусками
+  process.exit(code);
+});
+
+parcelProcess.on("error", err => {
+  console.error("Failed to start Parcel process:", err);
+  process.exit(1);
+});
